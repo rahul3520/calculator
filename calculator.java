c2f71ed599fa1d6eb7a28e2d51c5b8be72e10c2f
rahulg3520 <rahul@nestgroup.net>
2022-11-21 10:24:24 +0530

    main file

---
+++ calculator.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+public class calculator {
+
+    public static void main(String[] args) {
+
+        System.out.println("Select an operation:");
+
+        System.out.println("1:Addition \n 2:Subtraction \n 3:Multiplication \n 4:Division\n");
+
+        int choice=0;
+
+        System.out.println("Enter a choice:");
+
+        Scanner sc=new Scanner(System.in);
+
+        choice=sc.nextInt();
+
+        System.out.println("Enter 2 numbers:");
+
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+
+
+        switch(choice)
+        {
+            case 1: System.out.println(add(a,b));
+
+            case 2: System.out.println(sub(a,b));
+
+            case 3: System.out.println(mul(a,b));
+
+            case 4: System.out.println(div(a,b));
+
+            default: System.out.println("Wrong choice");
+        }
+
+    }
+}
