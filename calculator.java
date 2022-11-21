0b93734624703d9a45b84fcdab0d2718872be4a1
rahulg3520 <rahul@nestgroup.net>
2022-11-21 10:44:33 +0530

    div file

--- calculator.java
+++ calculator.java
@@ -17,6 +17,11 @@ public class calculator {
         return a*b;
     }
 
+    public static int div(int a,int b)
+    {
+        return a/b;
+    }
+
     public static void main(String[] args) {
 
         System.out.println("Select an operation:");
