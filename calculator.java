1ea8577460c0bcdee91d42d811751cf2c033f751
rahulg3520 <rahul@nestgroup.net>
2022-11-21 10:51:31 +0530

    break stmt added

--- calculator.java
+++ calculator.java
@@ -45,12 +45,16 @@ public class calculator {
         switch(choice)
         {
             case 1: System.out.println(add(a,b));
+                    break;
 
             case 2: System.out.println(sub(a,b));
+                    break;
 
             case 3: System.out.println(mul(a,b));
+                    break;
 
             case 4: System.out.println(div(a,b));
+                    break;
 
             default: System.out.println("Wrong choice");
         }
